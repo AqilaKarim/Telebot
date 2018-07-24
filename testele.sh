a79e2a5769d09bdf69f07781e6b476de7be4e4e2
Aqila F. Karim <32214317+AqilaKarim@users.noreply.github.com>
2018-07-24 15:06:22 +0700

    Update testele.sh

--- testele.sh
+++ testele.sh
@@ -4,7 +4,7 @@ scp aqila@192.168.44.227:/home/bot/chatid.txt /home/aqila
 
 while IFS= read line
 do
-  token="697263182:AAEljlmqD5wGKO1q6eSb6_Sn710gIOWey0s"
+  token="your_bot_token"
   msg="Test Message"
   curl -s -F chat_id=$line -F text="$msg" https://api.telegram.org/bot$token/sendMessage > /dev/null
 done < /home/aqila/chatid.txt
